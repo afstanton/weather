d3f74fe398c85413ee6988a27056ec89cb6e22c6
Aaron F Stanton <afstanton@gmail.com>
2025-03-20 00:14:40 -0400

    Docs, maybe should remove?

--- spec/requests/weather_spec.rb
+++ spec/requests/weather_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
+# All functionality for this exists in the service class.
+# This is just a stub to test the controller.
+# If there was any real functionality in the controller, it would be tested here.
 RSpec.describe "Weathers", type: :request do
-  describe "GET /show" do
+  describe "GET /" do
     it "returns http success" do
-      get "/weather/show"
+      get "/"
       expect(response).to have_http_status(:success)
     end
   end
